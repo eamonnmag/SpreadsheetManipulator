504087dca39bd3ceef7fc46132517346e6743615
Eamonn Maguire <eamonnmag@gmail.com>
2011-10-10 18:54:13 +0100

    Adding some functionalities to discover groups etc..

--- src/main/java/org/isatools/manipulator/SpreadsheetManipulation.java
+++ src/main/java/org/isatools/manipulator/SpreadsheetManipulation.java
@@ -1,7 +1,10 @@
 package org.isatools.manipulator;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.ListMultimap;
+
+import javax.swing.table.TableColumn;
+import java.util.*;
 
 /**
  * Provides a number of methods to manipulate the Spreadsheet representation (a List<String[]> where each List item
@@ -12,14 +15,13 @@ import java.util.List;
 public class SpreadsheetManipulation {
 
 
-
     /**
      * Return the column Headers
      *
      * @param fileContents - @see List<String[]> pertaining to the Rows and columns of the spreadsheet
      * @return the Column Headers, assuming that row 0 is where the column headers are positioned.
      */
-    public String[] getColumnHeaders(List<String[]> fileContents) {
+    public static String[] getColumnHeaders(List<String[]> fileContents) {
         return fileContents.get(0);
     }
 
@@ -27,10 +29,10 @@ public class SpreadsheetManipulation {
      * Given a number of row indices, e.g. 0, 3, 4, 6, 7 this method will return a subset of the sheet.
      *
      * @param fileContents -  @see List<String[]> pertaining to the Rows and columns of the spreadsheet
-     * @param rowIndices - a number of row indexes.
+     * @param rowIndices   - a number of row indexes.
      * @return - @see List<String[]> pertaining to the Rows and columns of the spreadsheet
      */
-    public List<String[]> getRowSubset(List<String[]> fileContents, int... rowIndices) {
+    public static List<String[]> getRowSubset(List<String[]> fileContents, int... rowIndices) {
 
         List<String[]> subSheet = new ArrayList<String[]>();
 
@@ -48,12 +50,13 @@ public class SpreadsheetManipulation {
 
     /**
      * Given a number of column indices, e.g. 0, 1, 4, 5, 8 this method will return a subset of the columns in the sheet and all rows.
-     * @param fileContents - @see List<String[]> pertaining to the Rows and columns of the spreadsheet
+     *
+     * @param fileContents         - @see List<String[]> pertaining to the Rows and columns of the spreadsheet
      * @param includeColumnHeaders - whether or not to return the column headers in the resulting sheet representation
-     * @param columnIndices - a number of column indices.
+     * @param columnIndices        - a number of column indices.
      * @return - @see List<String[]> pertaining to the Rows and columns of the spreadsheet
      */
-    public List<String[]> getColumnSubset(List<String[]> fileContents,boolean includeColumnHeaders, int... columnIndices) {
+    public static List<String[]> getColumnSubset(List<String[]> fileContents, boolean includeColumnHeaders, int... columnIndices) {
 
         List<String[]> subSheet = new ArrayList<String[]>();
 
@@ -70,8 +73,8 @@ public class SpreadsheetManipulation {
 
                     count++;
                 } else {
-                    throw new IndexOutOfBoundsException("You requested column " + columnIndex+ "  from the spreadsheet. " +
-                        "Unfortunately, this doesn't exist. Remember that column indices start at 0 and go up to ColumnNumber-1.");
+                    throw new IndexOutOfBoundsException("You requested column " + columnIndex + "  from the spreadsheet. " +
+                            "Unfortunately, this doesn't exist. Remember that column indices start at 0 and go up to ColumnNumber-1.");
                 }
             }
 
@@ -80,5 +83,74 @@ public class SpreadsheetManipulation {
         }
 
         return subSheet;
+    }
+
+    public static List<String[]> removeColumns(List<String[]> fileContents, int... columnIndicesToRemove) {
+        return null;
+    }
+
+    /**
+     * Groups up types of columns such as factors and returns the concatenation of those values on a per row basis.
+     *
+     * @param fileContents - Representation of file, like that emitted from the Loader. @see Loader
+     * @param group      - e.g. Factor, Characteristic
+     * @param exactMatch - if the Group should be an exact match to a table value (e.g. Factor could be any factor but Factor Value[Run Time] would be a perfect match.)
+     * @return ListMultimap<String, List<Object>> where list will be Strings if return Sample Names or Row indexes
+     */
+    public static Map<String, Set<String>> getDataGroupsByColumn(List<String[]> fileContents, String group, boolean exactMatch) {
+        Map<String, Set<String>> groups = new HashMap<String, Set<String>>();
+
+        String[] columnNames = SpreadsheetManipulation.getColumnHeaders(fileContents);
+
+        boolean allowedUnit = false;
+        for (int row = 1; row < fileContents.size(); row++) {
+            String groupVal = "";
+            for (int col = 0; col < columnNames.length; col++) {
+                String column = columnNames[col];
+
+                boolean match = false;
+
+                if (exactMatch) {
+                    if (column.equalsIgnoreCase(group)) {
+                        match = true;
+                    } else if (allowedUnit && column.equalsIgnoreCase("unit")) {
+                        match = true;
+                    }
+                } else {
+                    if (column.contains(group)) {
+                        match = true;
+                    } else if (allowedUnit && column.equalsIgnoreCase("unit")) {
+                        match = true;
+                    }
+                }
+
+                if (match) {
+                    groupVal += " " + fileContents.get(row)[col];
+                    allowedUnit = true;
+                } else allowedUnit = column.contains("Term Source REF") || column.contains("Term Accession Number");
+            }
+            if (!groupVal.equals("")) {
+                groupVal = groupVal.trim();
+
+                if(!groups.containsKey(groupVal)) {
+                    groups.put(groupVal, new HashSet<String>());
+                }
+
+                groups.get(groupVal).add(getColValAtRow(fileContents, columnNames, "Sample Name", row));
+            }
+        }
+
+        return groups;
+    }
+
+    public static String getColValAtRow(List<String[]> fileContents, String[] columnNames, String colName, int rowNumber) {
+        for (int col = 0; col < columnNames.length; col++) {
+
+            if (columnNames[col].equalsIgnoreCase(colName)) {
+                // safety precaution to finalise any cells. otherwise their value would be missed!
+                return fileContents.get(rowNumber)[col];
+            }
+        }
+        return "";
     }
 }
--- src/test/java/org/isatools/manipulator/SpreadsheetManipulationTest.java
+++ src/test/java/org/isatools/manipulator/SpreadsheetManipulationTest.java
@@ -1,11 +1,15 @@
 package org.isatools.manipulator;
 
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimap;
 import org.isatools.io.FileType;
 import org.isatools.io.Loader;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by the ISA team
@@ -35,9 +39,7 @@ public class SpreadsheetManipulationTest {
 
         System.out.println("Testing get column names");
 
-        SpreadsheetManipulation manipulation = new SpreadsheetManipulation();
-
-        String[] columnNames = manipulation.getColumnHeaders(load());
+        String[] columnNames = SpreadsheetManipulation.getColumnHeaders(load());
 
         for (String columnName : columnNames) {
             System.out.print(columnName + "\t");
@@ -48,12 +50,9 @@ public class SpreadsheetManipulationTest {
     public void testGetColumnSubset() {
 
         System.out.println("Testing get column subset");
-
-        SpreadsheetManipulation manipulation = new SpreadsheetManipulation();
-
         System.out.println();
 
-        List<String[]> subset = manipulation.getColumnSubset(load(), true, 0, 3, 4, 5);
+        List<String[]> subset = SpreadsheetManipulation.getColumnSubset(load(), true, 0, 3, 4, 5);
 
         for (String[] columns : subset) {
             for (String columnValue : columns) {
@@ -67,11 +66,9 @@ public class SpreadsheetManipulationTest {
     public void testRowManipulation() {
         System.out.println("Testing get row subset");
 
-        SpreadsheetManipulation manipulation = new SpreadsheetManipulation();
-
         System.out.println();
 
-        List<String[]> subset = manipulation.getRowSubset(load(), 0, 3, 4, 5);
+        List<String[]> subset = SpreadsheetManipulation.getRowSubset(load(), 0, 3, 4, 5);
 
         for (String[] columns : subset) {
             for (String columnValue : columns) {
@@ -81,4 +78,22 @@ public class SpreadsheetManipulationTest {
         }
     }
 
+    @Test
+    public void testGroups() {
+        System.out.println("Testing get groups");
+
+        System.out.println();
+
+        Map<String, Set<String>> subset = SpreadsheetManipulation.getDataGroupsByColumn(load(), "Factor", false);
+
+        System.out.println("Found " + subset.size() + " groups...");
+
+        for (String group : subset.keySet()) {
+            System.out.println("For group: " + group + " we have:");
+            for(String sample : subset.get(group)) {
+                System.out.println("\t" + sample);
+            }
+            System.out.println();
+        }
+    }
 }
